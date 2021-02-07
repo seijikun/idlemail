9fcb8440517412ce3042fd95e03ee728d6fc39bb
Markus Ebner <hiwatari.seiji@gmail.com>
2021-02-07 23:28:30 +0100

    Augment pretty logger with write-style env variable

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,21 @@ use log::{debug, error, info};
 use signal::{trap::Trap, Signal};
 use std::time::{Duration, Instant};
 
+fn init_logging() {
+    let mut log_builder = pretty_env_logger::formatted_builder();
+
+    if let Ok(level) = std::env::var("RUST_LOG") {
+        log_builder.parse_filters(&level);
+    }
+    if let Ok(write_style) = std::env::var("RUST_LOG_STYLE") {
+        log_builder.parse_write_style(&write_style);
+    }
+
+    log_builder.init();
+}
+
 fn main() {
-    pretty_env_logger::init();
+    init_logging();
 
     // dirty hack to read the config-file name from first argument for now
     let config_file = std::env::args().skip(1).take(1).next().unwrap();
